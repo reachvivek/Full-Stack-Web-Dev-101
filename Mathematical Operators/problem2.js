64c82fa6b7baabb144002803ab57428bd87a6878
Vivek Singh <rogerthatvivek@gmail.com>
2022-09-03 17:48:22 +0530

    Mathematical Operators

---
+++ Mathematical Operators/problem2.js
@@ -0,0 +1,7 @@
+let one = 1, two = 2, three = 3, four = 4, five = 5
+one = one * 2
+two = two * 2
+three = three * 2
+four = four * 2
+five = five * 2
+console.log(one+two+three+four+five)
---
+++ Mathematical Operators/problem3.js
@@ -0,0 +1,10 @@
+let one = 1, two = 2, three = 3, four = 4, five = 5, six = 6
+
+two = two * 2
+five = five * 2
+six = six * 2
+
+three = three * 3
+four = four * 3
+
+console.log(one+two+three+four+five+six)
