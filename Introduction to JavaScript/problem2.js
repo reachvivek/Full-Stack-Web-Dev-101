7128b0a3af8f96cd5c7b89a7a01ed25bbc7e2813
Vivek Singh <rogerthatvivek@gmail.com>
2022-09-03 17:43:30 +0530

    Introduction

---
+++ Introduction to JavaScript/problem2.js
@@ -0,0 +1,6 @@
+let name = "Vivek"
+console.log(name)
+name = "Vinay"
+console.log(name)
+name = "Manju"
+console.log(name)
---
+++ Introduction to JavaScript/problem4.js
@@ -0,0 +1,19 @@
+let name = "Vivek"
+let school = "DPS"
+let grade = "10"
+let section = "A"
+let rollno = 25
+let m1 = 95
+let m2 = 80
+let m3 = 85
+console.log("Report Card")
+console.log("Name : ", name)
+console.log("School : ", school)
+console.log("Grade : ", grade, " Section : ", section)
+console.log("Roll No : ", rollno)
+console.log("Marks")
+console.log("English : ", m1)
+console.log("Science : ", m2)
+console.log("Maths : ", m3)
+console.log("Result : A")
+console.log("Pass")
